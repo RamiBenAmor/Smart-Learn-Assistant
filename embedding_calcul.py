4ecd53bb2d319a30de6f3531b77e0974fc1f035c
RamiBenAmor <155146849+RamiBenAmor@users.noreply.github.com>
2025-05-09 12:26:24 +0100

    Update embedding_calcul.py

--- embedding_calcul.py
+++ embedding_calcul.py
@@ -4,8 +4,8 @@ import numpy as np
 import faiss
 from sentence_transformers import SentenceTransformer
 def generate_embeddings_and_faiss():
-  embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
-
+  embedding_model = SentenceTransformer("all-MiniLM-L6-v2")  #contextual embeddings 
+  # MiniLM, une version légère de BERT.
   with open("texts.pkl", "rb") as f:
     texts = pickle.load(f)
 
